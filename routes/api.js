0cfd3243ad2ef53874d3352a905c172c9043c464
Jack Woodrow <jackwoodrow99@gmail.com>
2022-02-10 14:00:39 -0500

    Remove index controller

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 // Controllers
-const IndexController = require('../controllers/IndexController');
 const ItemController = require('../controllers/ItemController');
 
-// Routes
-router.get('/', IndexController.testFcn);
-
+// Item routes
 router.get('/item', ItemController.index);
 router.post('/item', ItemController.create);
 router.patch('/item', ItemController.update);
+router.put('/item', ItemController.update);
 router.delete('/item', ItemController.remove);
+
+
 
 module.exports = router;
