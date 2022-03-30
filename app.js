7fffe4b5b83dc6675bed59e7abe9a2b2d442eb5c
Jack Woodrow <jackwoodrow99@gmail.com>
2022-03-30 09:10:15 -0400

    app

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
 
-async function Main(){
-    const app = express();
+async function Main() {
+	const app = express();
 
-    await require('./service/MongooseConnection')();
+	await require('./service/MongooseConnection')();
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser());
-    app.use(cors());
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+	app.use(cookieParser());
+	app.use(cors());
 
-    app.use('/', require('./routes/api'));
+	app.use('/', require('./routes/api'));
 
-    app.listen(process.env.PORT, () => { 
-        console.log(`Server started on port ${process.env.PORT}`);
-        console.log(`Avaliable on your local network at ${address.ip()}:${process.env.PORT}`);
-    });
+	app.listen(process.env.PORT, () => {
+		console.log(`Server started on port ${process.env.PORT}`);
+		console.log(
+			`Available on your local network at ${address.ip()}:${process.env.PORT}`
+		);
+	});
 }
 
 Main();
-
